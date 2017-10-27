b96437c944763002a2fa6ff9b807779c3ad7b1df
Calder Benjamin <calderbenjamin@gmail.com>
2017-10-27 09:57:43 -0600

    Added attributes to user and hub

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -9,11 +9,19 @@
 		<h1>Conceptual Model</h1>
 		<h2>User</h2>
 		<ul>
-			<li></li>
+			<li>userId (primary key)</li>
+			<li>userHubs</li>
+			<li>userImage</li>
+			<li>userName</li>
+			<li>userEmail</li>
+			<li>userReputation</li>
 		</ul>
 		<h2>Hub</h2>
 		<ul>
-			<li>oreo</li>
+			<li>hubId (primary key)</li>
+			<li>hubUserId (foreign key)</li>
+			<li>hubLocation</li>
+			<li>hubItems</li>
 		</ul>
 	</body>
 </html>
