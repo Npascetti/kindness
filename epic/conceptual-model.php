68a5ec1d57113c6b662a054ba512df24442e6db2
Calder Benjamin <calderbenjamin@gmail.com>
2017-10-27 10:06:43 -0600

    Created use cases outline and refined conceptual model

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -6,23 +6,25 @@
 	</head>
 
 	<body>
-		<h1>Conceptual Model</h1>
-		<h2>User</h2>
-		<ul>
-			<li>userId (primary key)</li>
-			<li>userHubs</li>
-			<li>userImage</li>
-			<li>userName</li>
-			<li>userEmail</li>
-			<li>userReputation</li>
-		</ul>
-		<h2>Hub</h2>
-		<ul>
-			<li>hubId (primary key)</li>
-			<li>hubUserId (foreign key)</li>
-			<li>hubLocation</li>
-			<li>hubItems</li>
-			<li>hubDateCreated</li>
-		</ul>
+		<main>
+			<h1>Conceptual Model</h1>
+			<h2>User</h2>
+			<ul>
+				<li>userId (primary key)</li>
+				<li>userHubs</li>
+				<li>userImage</li>
+				<li>userName</li>
+				<li>userEmail</li>
+				<li>userReputation</li>
+			</ul>
+			<h2>Hub</h2>
+			<ul>
+				<li>hubId (primary key)</li>
+				<li>hubUserId (foreign key)</li>
+				<li>hubLocation</li>
+				<li>hubItems</li>
+				<li>hubDateCreated</li>
+			</ul>
+		</main>
 	</body>
 </html>
---
+++ epic/use-cases.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Use Cases</title>
+	</head>
+
+	<body>
+		<main>
+
+		</main>
+	</body>
+</html>
