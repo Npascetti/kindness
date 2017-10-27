1afc8c199f2ab872501633487595270f1ca49251
Calder Benjamin <calderbenjamin@gmail.com>
2017-10-27 09:32:32 -0600

    Created title and navigation for index.php

---
+++ epic/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Operation: Kindness</title>
+	</head>
+
+	<body>
+		<header>
+			<h1>General overview</h1>
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li><a href="conceptual-model.php">Conceptual Model</a></li>
+					<li><a href="use-cases.php">Use Cases</a></li>
+				</ul>
+			</nav>
+		</header>
+	</body>
+</html>
