ea7ac6c306b464b45d3611936454bb60cdffb936
Calder Benjamin <calderbenjamin@gmail.com>
2017-10-27 09:37:35 -0600

    Created first draft of executive summary

--- epic/index.php
+++ epic/index.php
@@ -15,6 +15,17 @@
 					<li><a href="use-cases.php">Use Cases</a></li>
 				</ul>
 			</nav>
+
+			<main>
+				<h2>Executive Summary</h2>
+				<p>A service that allows users to set up and donate to hubs around the city. These hubs will
+					provide various supplies and goods to lower income and in need people. Users can either create
+					a profile to set up hubs on their own, and can appoint people to manage and oversee those hubs,
+					or can anonymously and easily donate to the hubs. The hubs will have centralized locations so that
+					those without easy internet access will know how to access them. The hubs will take in anything
+					users are willing to donate, and can supply everything from food to blankets and clothes and
+					everything inbetween.</p>
+			</main>
 		</header>
 	</body>
 </html>
