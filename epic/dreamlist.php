3f2cf2242ba6b7d4f4a1d0e253b9975d6d5962fc
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-27 12:42:08 -0600

    created dreamlist.php

---
+++ epic/dreamlist.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Dreamlist</title>
+		<meta charset="utf-8"/>
+	</head>
+
+	<body>
+		<ul>
+			<li>Integration with public/private infrastructure and business- Restaurant hubs</li>
+			<li>Social media sharing</li>
+			<li>Google/facebook signin</li>
+			<li>Increase hub locations</li>
+			<li>Expand services outside of food - portable showers, other services</li>
+			<li>Address affiliation???</li>
+			<li>User to user interaction</li>
+		</ul>
+	</body>
