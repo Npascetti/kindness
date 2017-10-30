83dafa864911f5fb7e500f8887e0299010fb6d6e
Michael Romero <m.romero1989@gmail.com>
2017-10-30 14:18:16 -0600

    Added bio and shit for shannon. lol

--- epic/shannon.php
+++ epic/shannon.php
@@ -22,6 +22,27 @@
 
 		<main>
 			<img class="resize" src="images/shannonphoto.jpg" alt="Shannon's Persona">
+			<h2>Bio</h2>
+			<ul>
+				<li>Age: 40</li>
+				<li>Occupation: HR Manager</li>
+				<li>Location: NE Heights, ABQ</li>
+				<li>Archetype: Yoga + Whole Foods Mom</li>
+				<li>Technology: iPhone User, iMac, and SMART T.V.</li>
+			</ul>
+			<p>It is in Shannon's nature to help others. She is an active member of several community outreach groups. She engages in community service along with her two daughters often. Shannon became dissatisfied working with previous nonprofit organizations. The large structure, actions, and questionable ethical choices were what turned her off. She believes those issues could be fixed by a more personal approach. In short, she thinks that direct donation and assistance is the best way for her to help. </p>
+			<h2>Goals</h2>
+			<ul>
+				<li>Help those in need.</li>
+				<li>Make a direct impact<./li>
+				<li>Be an active member of a beneficial social media site.</li>
+			</ul>
+			<h2>Frustrations</h2>
+			<ul>
+				<li>Bureaucracy in non-profits.</li>
+				<li>Seeing hungry people on the streets.</li>
+				<li>Feels empathetic for those down on their luck.</li>
+			</ul>
 		</main>
 	</body>
 </html>
