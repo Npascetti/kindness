c5a23d7dd5a7f1a4eb6334bc1e917f7a18b1cc5f
Jermain Jennings <jermainejennings05@gmail.com>
2017-10-30 14:16:58 -0600

    Typos fixed on Reoberts persona

--- epic/robert.php
+++ epic/robert.php
@@ -26,21 +26,21 @@
 					<li>Age: 30</li>
 					<li>Occupation: Entrepreneur</li>
 					<li>Location: Northwest Albuquerque</li>
-					<li>Archetypr: Upper Class Soccer Dad </li>
-					<li>Tech: iPhone X and Wiondows surrface running windows10</li>
+					<li>Archetype: Upper Class Soccer Dad </li>
+					<li>Tech: iPhone X and Windows surface running windows10</li>
 				</ul>
 			<p>Robert grew up in a single parent household where he didn't get to eat every meal. His mother worked hard
 				and he saw the struggle his mother went through. He now is in a position to give back to the local Albuquerque
-				community, specifically to those in his priior position. He wants to teach his kids the value in giving to those who dont have.
+				community, specifically to those in his prior position. He wants to teach his kids the value in giving to those who don't have.
 				Being that Robert is a very busy man with work and family he doesn't have a lot of time to jump through hoops
 				and obstacles to donate and give back. Robert uses this app to donate simply,fast, and on a regular basis. </p>
 			<h2>Goals</h2>
 				<ul>
 					<li>Donate fast and often</li>
-					<li>Build chracter for his kids</li>
+					<li>Build character for his kids</li>
 					<li>Wants to be in control of his donations</li>
 				</ul>
-			<h2>Fustrations</h2>
+			<h2>Frustrations</h2>
 				<ul>
 					<li>Consistently running out of time to donate</li>
 					<li>Not knowing where to donate</li>
