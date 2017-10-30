e7b13f1cff6e4df798387141ad4850a8df0716ce
Jermain Jennings <jermainejennings05@gmail.com>
2017-10-30 14:07:36 -0600

    Roberts persona edited

--- epic/robert.php
+++ epic/robert.php
@@ -19,9 +19,33 @@
 				</ul>
 			</nav>
 		</header>
-
+		<img src="images" alt="robert.jpg">
 		<main>
-			<img class="resize" src="./images/robert.png" alt="Robert Persona">
+			<h2>Bio</h2>
+				<ul>
+					<li>Age: 30</li>
+					<li>Occupation: Entrepreneur</li>
+					<li>Location: Northwest Albuquerque</li>
+					<li>Archetypr: Upper Class Soccer Dad </li>
+					<li>Tech: iPhone X and Wiondows surrface running windows10</li>
+				</ul>
+			<p>Robert grew up in a single parent household where he didn't get to eat every meal. His mother worked hard
+				and he saw the struggle his mother went through. He now is in a position to give back to the local Albuquerque
+				community, specifically to those in his priior position. He wants to teach his kids the value in giving to those who dont have.
+				Being that Robert is a very busy man with work and family he doesn't have a lot of time to jump through hoops
+				and obstacles to donate and give back. Robert uses this app to donate simply,fast, and on a regular basis. </p>
+			<h2>Goals</h2>
+				<ul>
+					<li>Donate fast and often</li>
+					<li>Build chracter for his kids</li>
+					<li>Wants to be in control of his donations</li>
+				</ul>
+			<h2>Fustrations</h2>
+				<ul>
+					<li>Consistently running out of time to donate</li>
+					<li>Not knowing where to donate</li>
+					<li>Lack of public resources</li>
+				</ul>
 		</main>
 	</body>
 </html>
