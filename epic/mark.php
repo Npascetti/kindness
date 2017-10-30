4b84c073c88de72836b5685193e4b1287019f9b2
Marcus Caldeira <mac.caldr@gmail.com>
2017-10-30 14:00:11 -0600

    mark bio

---
+++ epic/mark.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mark Persona</title>
+    <meta charset="utf-8"/>
+    <link type="text/css" href="css/robertstyle.css" rel="stylesheet" />
+</head>
+
+<body>
+<header>
+    <h1>Robert Lopez</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="conceptual-model.php">Conceptual Model</a></li>
+            <li><a href="use-cases.php">Use Cases</a></li>
+            <li><a href="shannon.php">Shannon Persona</a></li>
+            <li>Robert Persona</li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <img class="epic/images/markAlbina.jpg">
+    <p>Mark has been employed at Burger King for two years and he lives in Barelas. He commutes on his bicycle with a ten
+    ride to Lomas. His manager will only give him a part time position and has no other formal training to look for another
+    job. He makes ends meet, but consistently pays child support for his two daughters that live in Texas. The payments are 90%
+    of his income. He is forced to skip dinner, he can't make trips to the laundromat as often sd he'd like, and continuously
+     wont have enough money to buy a full weeks worth of groceries. Mark just can't seem to make time for government assistance,
+     he works during the day. Mark remains positive and knows that he can makes things work if he just had a little help.<p>
+</main>
+</body>
+</html>
