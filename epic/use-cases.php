e06d5a697ebd92414887d1d5518b308e1a8b6939
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-31 09:00:33 -0600

    added blacklist caveat to shannon use case

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -48,8 +48,8 @@
 			<ol>
 				<li>Shannon opens up the website</li>
 				<li>Website displays home page</li>
-				<li>Shannon clicks on create new hub</li>
-				<li>Website takes Shannon to hub creation page</li>
+				<li>Shannon clicks on create new hub.</li>
+				<li>Website takes Shannon to hub creation page as long as her reputation level is not below blacklist level.</li>
 				<li>Shannon creates a new hub</li>
 				<li>Website displays home page, now with Shannon's new hub displayed</li>
 			</ol>
