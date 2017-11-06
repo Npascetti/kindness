00e10dd6e6e9ceb1389e6b9f55cba5d642f07310
Nick Pascetti <npascetti@cnm.edu>
2017-11-05 18:30:24 -0700

    created file, added namespace, requires, and opening docblock for User.php 467416618315421/470659655215547

---
+++ User.php
@@ -0,0 +1,13 @@
+<?php
+namespace EDU\Cnm\KindHub
+
+require_once("autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+/**
+ * This is the User entity. This represents everything needed for a user and their profile to exist, as well as some other extra info, such as bio and image.
+ *
+ * @author Nicklas Pascetti, Dylan McDonald <npascetti@gmail.com> <dmcdonald21@cnm.edu>
+ * @version 1.0.0
+ **/
+?>
