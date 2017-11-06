c7b111102a5f63dbf9a6535d206bcd892138ed04
Michael Romero <m.romero1989@gmail.com>
2017-11-05 19:32:38 -0700

    Added accessors and mutators for reputationHubId, reputationLevelId, reputation user id and reputation point...I might need to change stuff on reputation point.

--- php/classes/Reputation.php
+++ php/classes/Reputation.php
@@ -9,8 +9,8 @@ use Ramsey\Uuid\Uuid;
 
 /**
  * This is the Reputation Entity. This entity handles reputation of the user and hub.
- * @author Michael Romeor <dmcdonald21@cnm.edu>
- * @version 3.0.0
+ * @author Michael Romeor <m.romero1989@gmail.com>
+ * @version 1.0.0
  **/
 
 class Reputation implements \JsonSerializable {
@@ -42,7 +42,6 @@ class Reputation implements \JsonSerializable {
 	 */
 	private $reputationPoint;
 
-
 	/**
 	 * constructor for Reputation
 	 *
@@ -70,6 +69,152 @@ class Reputation implements \JsonSerializable {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
+	}
+
+	/**
+	 * accessor method for reputation id
+	 *
+	 * @return Uuid value of reputation id
+	 **/
+	public function getReputationId() : Uuid {
+		return($this->reputationId);
+	}
+
+	/**
+	 * mutator method for tweet id
+	 *
+	 * @param Uuid/string $newReputationId new value of reputation id
+	 * @throws \RangeException if $newReputationId is not positive
+	 * @throws \TypeError if $newReputationId is not a uuid or string
+	 **/
+	public function setReputationId( $newReputationId) : void {
+		try {
+			$uuid = self::validateUuid($newReputationId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		// convert and store the reputation id
+		$this->reputationId = $uuid;
+	}
+
+	/**
+	 * accessor method for reputation hub id
+	 *
+	 * @return Uuid value of reputation hub id
+	 **/
+	public function getReputationHubId() : Uuid{
+		return($this->reputationHubId);
+	}
+
+	/**
+	 * mutator method for reputation hub id
+	 *
+	 * @param string | Uuid $newReputationHubId new value of reputationHubId
+	 * @throws \RangeException if $newReputationHubId is not positive
+	 * @throws \TypeError if $newReputationHubId is not an integer
+	 **/
+	public function setReputationHubId( $newReputationHubId) : void {
+		try {
+			$uuid = self::validateUuid($newReputationHubId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		// convert and store the reputation hub id
+		$this->reputationHubId = $uuid;
+	}
+
+	/**
+	 * accessor method for reputation level id
+	 *
+	 * @return Uuid value of reputation level id
+	 **/
+	public function getReputationLevelId() : Uuid {
+		return($this->reputationLevelId);
+	}
+
+	/**
+	 * mutator method for reputation level id
+	 *
+	 * @param Uuid/string $newReputationLevelId new value of reputation level id
+	 * @throws \RangeException if $newReputationLevelId is not positive
+	 * @throws \TypeError if $newReputationLevelId is not a uuid or string
+	 **/
+	public function setReputationLevelId( $newReputationLevelId) : void {
+		try {
+			$uuid = self::validateUuid($newReputationLevelId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		// convert and store the reputation level id
+		$this->reputationLevelId = $uuid;
+	}
+
+	/**
+	 * accessor method for reputation user id
+	 *
+	 * @return Uuid value of reputation user id
+	 **/
+	public function getReputationUserId() : Uuid {
+		return($this->reputationUserId);
+	}
+
+	/**
+	 * mutator method for reputation user id
+	 *
+	 * @param Uuid/string $newReputationUserId new value of reputation user id
+	 * @throws \RangeException if $newReputationUserId is not positive
+	 * @throws \TypeError if $newReputationUserId is not a uuid or string
+	 **/
+	public function setReputationUserId( $newReputationUserId) : void {
+		try {
+			$uuid = self::validateUuid($newReputationUserId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		// convert and store the reputation user id
+		$this->reputationUserId = $uuid;
+	}
+
+	/**
+	 * accessor method for reputation point
+	 *
+	 * @return string value of reputation point
+	 **/
+	public function getReputationPoint() :string {
+		return($this->reputationPoint);
+	}
+
+	/**
+	 * mutator method for reputation point
+	 *
+	 * @param string $newReputationPoint new value of reputation point
+	 * @throws \InvalidArgumentException if $newReputationPoint is not a string or insecure
+	 * @throws \RangeException if $newReputationPoint is > 140 characters
+	 * @throws \TypeError if $newReputationPoint is not a string
+	 **/
+	public function setReputationPoint(string $newReputationPoint) : void {
+		// verify the reputation point is secure
+		$newReputationPoint = trim($newReputationPoint);
+		$newReputationPoint = filter_var($newReputationPoint, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newReputationPoint) === true) {
+			throw(new \InvalidArgumentException("reputation point is empty or insecure"));
+		}
+
+		// verify the reputation point will fit in the database
+		if(strlen($newReputationPoint) > 140) {
+			throw(new \RangeException("reputation point too large"));
+		}
+
+		// store the reputation point
+		$this->reputationPoint = $newReputationPoint;
 	}
 
 }
