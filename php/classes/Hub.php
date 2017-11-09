9e46eda39862ea4c736d43e9b57ee0d7527923f8
Calder Benjamin <calderbenjamin@gmail.com>
2017-11-09 16:42:47 -0700

    Added more thorough commenting on Hub class https://app.asana.com/0/467416618315421/470659655215549

--- php/classes/Hub.php
+++ php/classes/Hub.php
@@ -57,6 +57,7 @@ class Hub implements \JsonSerializable {
 	 * @throws \Exception if some other exception occurs
 	 **/
 	public function __construct($newHubId, $newHubUserId, string $newHubLocation, string $newHubName) {
+		// Sets all necessary parameters of the hub and throws an exception if any are out of bounds or a type error
 		try {
 			$this->setHubId($newHubId);
 			$this->setHubUserId($newHubUserId);
@@ -83,12 +84,14 @@ class Hub implements \JsonSerializable {
 	 * @param Uuid $newHubId The new value of the hub ID
 	 **/
 	public function setHubId($newHubId): void {
+		// Makes sure the Uuid is valid, throws an exception if it is not
 		try {
 			$uuid = self::validateUuid($newHubId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
+		// If the try statement executed, set the hubId
 		$this->hubId = $uuid;
 	}
 
@@ -107,12 +110,14 @@ class Hub implements \JsonSerializable {
 	 * @param Uuid $newHubUserId The new ID of the hub's creator
 	 **/
 	public function setHubUserId($newHubUserId): void {
+		// Makes sure the Uuid is valid, throws an exception if it is not
 		try {
 			$uuid = self::validateUuid($newHubUserId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
+		// If the try statement executed, set the hubUserId
 		$this->hubUserId = $uuid;
 	}
 
@@ -131,12 +136,15 @@ class Hub implements \JsonSerializable {
 	 * @param string $newHubLocation the new hub location
 	 **/
 	public function setHubLocation($newHubLocation): void {
+		// Trims and then filters the Location
 		$newHubLocation = trim($newHubLocation);
 		$newHubLocation = filter_var($newHubLocation, FILTER_SANITIZE_STRING,
 			FILTER_FLAG_NO_ENCODE_QUOTES);
+		// Checks if the filtered string was valid
 		if(empty($newHubLocation)) {
 			throw(new \InvalidArgumentException("Location is empty or insecure"));
 		}
+		// If the string was valid, sets the hubLocation
 		$this->hubLocation = $newHubLocation;
 	}
 
@@ -155,12 +163,15 @@ class Hub implements \JsonSerializable {
 	 * @param string $newHubName The new name of the hub
 	 **/
 	public function setHubName($newHubName): void {
+		// Trims and then filters the Name
 		$newHubName = trim($newHubName);
 		$newHubName = filter_var($newHubName, FILTER_SANITIZE_STRING,
 		FILTER_FLAG_NO_ENCODE_QUOTES);
+		// Checks if the filtered string was valid
 		if(empty($newHubName)) {
 			throw(new \InvalidArgumentException("Name is empty or insecure"));
 		}
+		// If the string was valid, sets the hubName
 		$this->hubName = $newHubName;
 	}
 
@@ -172,10 +183,12 @@ class Hub implements \JsonSerializable {
 	 * @throws \TypeError when $pdo is not a PDO connection object
 	 **/
 	public function insert(\PDO $pdo): void {
+		// Formats the insert statement
 		$query = "INSERT INTO hub(hubId, hubUserId, hubLocation, hubName) 
 			VALUES (:hubId, :hubUserId, :hubLocation, :hubName)";
 		$statement = $pdo->prepare($query);
 
+		// Sets the parameters and executes the insert statement
 		$parameters = ["hubId" => $this->hubId->getBytes(), "hubUserId" => $this->hubUserId->getBytes(),
 			"hubLocation" => $this->hubLocation, "hubName" => $this->hubName];
 		$statement->execute($parameters);
@@ -189,9 +202,11 @@ class Hub implements \JsonSerializable {
 	 * @throws \TypeError if $pdo is not a PDO connection object
 	 **/
 	public function delete(\PDO $pdo): void {
+		// Formats the delete statement
 		$query = "DELETE FROM hub WHERE hubId = :hubId";
 		$statement = $pdo->prepare($query);
 
+		// Sets the parameters and executes the delete statement
 		$parameters = ["hubId" => $this->hubId->getBytes()];
 		$statement->execute($parameters);
 	}
@@ -204,10 +219,12 @@ class Hub implements \JsonSerializable {
 	 * @throws \TypeError if $pdo is not a PDO connection object
 	 **/
 	public function update(\PDO $pdo): void {
+		// Formats the update statement
 		$query = "UPDATE hub SET hubUserId = :hubUserId, hubLocation = :hubLocation, hubName = :hubName 
 			WHERE hubId = :hubId";
 		$statement = $pdo->prepare($query);
 
+		// Sets the parameters and executes the statement
 		$parameters = ["hubId" => $this->hubId->getBytes(), "hubUserId" => $this->hubUserId->getBytes(),
 			"hubLocation" => $this->hubLocation, "hubName" => $this->hubName];
 		$statement->execute($parameters);
@@ -223,18 +240,22 @@ class Hub implements \JsonSerializable {
 	 * @throws \TypeError when a variable are not the correct data type
 	 **/
 	public static function getHubByHubId(\PDO $pdo, $hubId): ?hub {
+		// Checks if the Uuid is valid and throws an exception if it isn't
 		try {
 			$hubId = self::validateUuid($hubId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
 
+		// Formats the select statement
 		$query = "SELECT hubId, hubUserId, hubLocation, hubName FROM hub WHERE hubId = :hubId";
 		$statement = $pdo->prepare($query);
 
+		// Sets the parameters and executes the select statement
 		$parameters = ["hubId" => $hubId->getBytes()];
 		$statement->execute($parameters);
 
+		// Checks if any hubs in mySQL match the hubId given, and creates a new hub object if one does
 		try {
 			$hub = null;
 			$statement->setFetchMode(\PDO::FETCH_ASSOC);
@@ -245,6 +266,7 @@ class Hub implements \JsonSerializable {
 		} catch(\Exception $exception) {
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
+		// Returns the hub if found or null if not
 		return($hub);
 	}
 
@@ -258,20 +280,26 @@ class Hub implements \JsonSerializable {
 	 * @throws \TypeError when a variable are not the correct data type
 	 **/
 	public static function getHubsByHubUserId(\PDO $pdo, $hubUserId): \SplFixedArray {
+		// Checks if the Uuid is valid and throws an exception if it isn't
 		try {
 			$hubUserId = self::validateUuid($hubUserId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
 
+		// Formats the select statement
 		$query = "SELECT hubId, hubUserId, hubLocation, hubName FROM hub WHERE hubUserId = :hubUserId";
 		$statement = $pdo->prepare($query);
 
+		//Sets the parameters and executes the select statement
 		$parameters = ["hubUserId" => $hubUserId->getBytes()];
 		$statement->execute($parameters);
 
+		// Creates an SplFixedArray to hold multiple hubs
 		$hubs = new \SplFixedArray($statement->rowCount());
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+
+		// Checks if any hubs in mySQL match the hubUserId given, and creates a new hub object and adds it to the array if it does
 		while(($row = $statement->fetch()) !== false) {
 			try {
 				$hub = new Hub($row["hubId"], $row["hubUserId"], $row["hubLocation"], $row["hubName"]);
@@ -281,6 +309,7 @@ class Hub implements \JsonSerializable {
 				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
 		}
+		// Returns the array of hubs, which is empty if none are found
 		return($hubs);
 	}
 
@@ -288,38 +317,47 @@ class Hub implements \JsonSerializable {
 	 * Gets the hub by the hub name
 	 *
 	 * @param \PDO $pdo PDO connection object
-	 * @param string $hubName the name of the hub
-	 * @return hub|null the hub or null if not found
+	 * @param string $hubName the name of the hub to search for
+	 * @return \SplFixedArray SplFixedArray of hubs found or null if none found
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError when a variable are not the correct data type
 	 **/
-		public static function getHubByHubName(\PDO $pdo, string $hubName): ?hub {
+	public static function getHubByHubName(\PDO $pdo, string $hubName): \SplFixedArray {
+		// Trims and filters the given hubName, and checks that it is still valid
 		$hubName = trim($hubName);
 		$hubName = filter_var($hubName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		if(empty($hubName)) {
 			throw(new \PDOException("Hub name is invalid or empty"));
 		}
 
+		// Formats the hub name to work with special characters
 		$hubName = str_replace("_", "\\_", str_replace("%", "\\%", $hubName));
 
+		// Formats the select statement
 		$query = "SELECT hubId, hubUserId, hubLocation, hubName FROM hub WHERE hubName LIKE :hubName";
 		$statement = $pdo->prepare($query);
 
+		// Allows for the statement to return any name that contains the search anywhere, sets the parameters, and executes the statement
 		$hubName = "%$hubName%";
 		$parameters = ["hubName" => $hubName];
 		$statement->execute($parameters);
 
-		try {
-			$hub = null;
-			$statement->setFetchMode(\PDO::FETCH_ASSOC);
-			$row = $statement->fetch();
-			if($row !== false) {
+		// Creates an SplFixedArray to hold multiple hubs
+		$hubs = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+
+		// Checks if any hubs in mySQL match the hubName given, and creates a new hub object and adds it to the array if it does
+		while(($row = $statement->fetch()) !== false) {
+			try {
 				$hub = new Hub($row["hubId"], $row["hubUserId"], $row["hubLocation"], $row["hubName"]);
+				$hub[$hubs->key()] = $hub;
+				$hubs->next();
+			} catch(\Exception $exception) {
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
-		} catch(\Exception $exception) {
-			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
-		return($hub);
+		// Returns the array of hubs, which is empty if none are found
+		return($hubs);
 	}
 
 	/**
@@ -331,12 +369,16 @@ class Hub implements \JsonSerializable {
 	 * @throws \TypeError when a variable are not the correct data type
 	 **/
 	public static function getAllHubs(\PDO $pdo): \SplFixedArray {
+		// Formats and executes the select statement
 		$query = "SELECT hubId, hubUserId, hubLocation, hubName FROM hub";
 		$statement = $pdo->prepare($query);
 		$statement->execute();
 
+		//Creates an SplFixedArray to hold the hubs
 		$hubs = new \SplFixedArray($statement->rowCount());
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+
+		// Gets all existing hubs and places them in the array
 		while(($row = $statement->fetch()) !== false) {
 			try {
 				$hub = new Hub($row["hubId"], $row["hubUserId"], $row["hubLocation"], $row["hubName"]);
@@ -346,6 +388,7 @@ class Hub implements \JsonSerializable {
 				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
 		}
+		// Returns the array of hubs
 		return($hubs);
 	}
 
