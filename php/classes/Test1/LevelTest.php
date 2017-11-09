b022ff6072ad3d17a8c8323c1aa721b19e6a0046
Jermain Jennings <jermainejennings05@gmail.com>
2017-11-09 13:34:29 -0700

    Unit test started for level. https://app.asana.com/0/467416618315421/470659655215551

---
+++ php/classes/Test1/LevelTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * Full PHPUnit test for the Tweet class
+ *
+ * This is a complete PHPUnit test of the Level class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Tweet
+ * @author Jermain Jennings
+ **/
+
