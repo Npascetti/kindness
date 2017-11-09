4cac86b4ace8207a1e9658a6c28a57e98d2dada5
Jermain Jennings <jermainejennings05@gmail.com>
2017-11-09 16:07:41 -0700

    valid and invalid parameters added for levelname and level number. https://app.asana.com/0/467416618315421/470659655215551

--- php/classes/Test1/LevelTest.php
+++ php/classes/Test1/LevelTest.php
@@ -1,19 +1,45 @@
 <?php
-/**
- *
- * Full PHPUnit test for the Tweet class
- *
- * This is a complete PHPUnit test of the Level class. It is complete because *ALL* mySQL/PDO enabled methods
- * are tested for both invalid and valid inputs.
- *
- * @see Tweet
- * @author Jermain Jennings
- **/
-class LevelTest extends KindnessTest {
 	/**
-	 *  this is for foreign key relations
-	 * @var level $level
+ 	*
+   * Full PHPUnit test for the Tweet class
+   *
+ 	* This is a complete PHPUnit test of the Level class. It is complete because *ALL* mySQL/PDO enabled methods
+ 	* are tested for both invalid and valid inputs.
+	 *
+	 * @see Tweet
+	 * @author Jermain Jennings
 	 **/
-	protected $level = null;
+
+	class LevelTest extends KindHubTest {
+
+		/**
+		 *  this is for foreign key relations
+		 * @var level $level
+		 **/
+		protected $level = null;
+
+		/**
+		 * Name of the LEVELNAME
+		 * @var string $VALID_LEVELNAME
+		 */
+		protected $VALID_LEVELNAME = "seven eleven";
+
+		/**
+		 * Name of the LEVELNAME
+		 * @var string $INVALID_LEVELNAME
+		 */
+		protected $INVALID_LEVELNAME = "null" ;
+
+		/**
+		 * Number of the LEVELNUMBER
+		 * @var string $INVALID_LEVELNUMBER
+		 */
+		protected $VALID_LEVELNUMBER = "8";
+
+		/**
+		 * Number of the LEVELNUMBER
+		 * @var string $INVALID_LEVELNUMBER
+		 */
+		protected $INVALID_LEVELNUMBER = "nan" ;
 
 
