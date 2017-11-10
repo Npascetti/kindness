c3e23e6270e016031251798ab5693d56ddfc833c
Jermain Jennings <jermainejennings05@gmail.com>
2017-11-10 09:05:13 -0700

    https://app.asana.com/0/467416618315421/470659655215551

--- php/classes/Test1/LevelTest.php
+++ php/classes/Test1/LevelTest.php
@@ -28,11 +28,11 @@
 		 * Name of the LEVELNAME
 		 * @var string $INVALID_LEVELNAME
 		 */
-		protected $INVALID_LEVELNAME = "null" ;
+		protected $INVALID_LEVELNAME = "null";
 
 		/**
 		 * Number of the LEVELNUMBER
-		 * @var string $INVALID_LEVELNUMBER
+		 * @var string $VALID_LEVELNUMBER
 		 */
 		protected $VALID_LEVELNUMBER = "8";
 
@@ -40,6 +40,34 @@
 		 * Number of the LEVELNUMBER
 		 * @var string $INVALID_LEVELNUMBER
 		 */
-		protected $INVALID_LEVELNUMBER = "nan" ;
+		protected $INVALID_LEVELNUMBER = "nan";
+
+		/**
+		 * Id of the LEVELID
+		 * @var string $VALID_LEVELID
+		 */
+		protected $VALID_LEVELID = "";
+
+		/**
+		 * Id of the LEVELID
+		 * @var string $VINALID_LEVELID
+		 */
+		protected $INVALID_LEVELID = "";
 
 
+		/**
+		 * Tests inserting a valid level into mySQL and verifying the data in mySQL matches
+		 **/
+		public function testInsertValidHub(): void {
+			$numRows = $this->getConnection()->getRowCount("level");
+
+			$levelId = generateUuidV4();
+			$level = new Level($levelId, $this->user->getUserId(), $this->VALID_LEVELNAME, $this->VALID_NUMBER);
+			$level->insert($this->getPDO());
+
+			$pdoLevel = Hub::getlevelByUserId($this->getPDO(), $level->getlevelId());
+			$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("level"));
+			$this->assertEquals($pdoLevel->getLevelId(), $levelId);
+			$this->assertEquals($pdoLevel->getHubUserId(), $this->user->getUserId());
+			$this->assertEquals($pdoHub->getHubLocation(), $this->VALID_HUBLOCATION);
+			$this->assertEquals($pdoHub->getHubName(), $this->VALID_HUBNAME);
