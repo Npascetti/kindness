dabcd6ee2f2c575b4801f224e02fa50bc7e590b2
Nicklas Pascetti <Npascetti@gmail.com>
2017-11-10 09:04:38 -0700

    wrote setup for UserTest.php https://app.asana.com/0/467416618315421/470659655215548

--- php/classes/Test1/UserTest.php
+++ php/classes/Test1/UserTest.php
@@ -16,10 +16,70 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
  *
  * This is a complete PHPUnit test of the User class. All mySQL/PDO enabled methods are tested for both invalid and valid inputs.
  *
+ * @see User
  * @author Dylan McDonald, Nick Pascetti, Marcus Caldeira <dmcdonald@cnm.edu> <npascetti@gmail.com> <mac.caldr@gmail.com>
  **/
 class UserTest extends KindHubTest {
+	/**
+	 * placeholder until account activation is created
+	 * @var string $VALID_ACTIVATIONTOKEN
+	 **/
+	protected $VALID_ACTIVATIONTOKEN;
 
+	/**
+	 * valid user bio
+	 * @var string $VALID_BIO
+	 **/
+	protected $VALID_BIO = "I love giving people stuff";
+
+	/**
+	 * second valid bio
+	 * @var string $VALID_BIO2
+	 **/
+	protected $VALID_BIO2 = "My mom makes me donate stuff.";
+
+	/**
+	 * valid email to use
+	 * @var string $VALID_EMAIL
+	 **/
+	protected $VALID_EMAIL = "nicklovesdonating@nicky.com";
+
+	/**
+	 * valid first name to use
+	 * @var string $VALID_FIRSTNAME
+	 **/
+	protected $VALID_FIRSTNAME = "Nicky";
+
+	/**
+	 * valid hash to use
+	 * @var $VALID_HASH
+	 **/
+
+	protected $VALID_HASH;
+
+	/**
+	 * valid user image link to use
+	 * @var string $VALID_IMAGE
+	 **/
+	protected $VALID_IMAGE = "https://i.pinimg.com/564x/32/7f/d4/327fd4661edc6e1862d29e37adf2648a--stoner-art-t-rex.jpg";
+
+	/**
+	 * valid user last name to use
+	 * @var string $VALID_LASTNAME
+	 **/
+	protected $VALID_LASTNAME = "Spaghetti";
+
+	/**
+	 * valid user user name to use
+	 * @var string $VALID_USERNAME
+	 **/
+	protected $VALID_USERNAME = "PascettiSpaghetti";
+
+	/**
+	 * valid salt to use to create the user pobject to own the test
+	 * @var string $VALID_SALT
+	 **/
+	protected $VALID_SALT;
 }
 
 
