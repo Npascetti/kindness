7f29a34902c92492a6fc3c54cf9bd80871a7ac7e
Calder Benjamin <calderbenjamin@gmail.com>
2017-11-10 11:39:53 -0700

    Deleted all created hubs at the end of relevant tests https://app.asana.com/0/467416618315421/470659655215549

--- php/classes/Test/HubTest.php
+++ php/classes/Test/HubTest.php
@@ -8,7 +8,7 @@ use Ramsey\Uuid\Uuid;
 
 require_once(dirname(__DIR__)."autoload.php");
 require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
-require_once(dirname(__DIR__, 2) . "vendor/autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 /**
  * Full PHPUnit Test for the Hub Class
