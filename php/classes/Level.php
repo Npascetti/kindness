7b1056a37c61cf6f943848b4ff6426563e4d4d4e
Calder Benjamin <calderbenjamin@gmail.com>
2017-11-13 15:15:11 -0700

    Added namespace and autoload to Level

--- php/classes/Level.php
+++ php/classes/Level.php
@@ -1,5 +1,12 @@
 <?php
 
+namespace Edu\Cnm\KindHub;
+
+require_once("autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+
 /**
  * Created by PhpStorm.
  * User: jermainejennings
