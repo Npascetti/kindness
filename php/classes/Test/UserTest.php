73c121e5dccdac2cc354d4fa2ac22842f7f92d40
Calder Benjamin <calderbenjamin@gmail.com>
2017-11-14 13:17:29 -0700

    Added vendor/autoload to UserTest

--- php/classes/Test/UserTest.php
+++ php/classes/Test/UserTest.php
@@ -12,6 +12,8 @@ require_once(dirname(__DIR__) . "/autoload.php");
 
 // grab the uuid generator
 require_once(dirname(__DIR__) . "/lib/uuid.php");
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+
 
 /**
  * Full PHPUnit test for the User class
