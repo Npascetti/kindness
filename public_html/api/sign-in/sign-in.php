59eeb54aa963750505894dd8a2120fd051f1e5f7
Jermain Jennings <jermainejennings05@gmail.com>
2017-11-16 15:27:44 -0700

    https://app.asana.com/0/467416618315421/481713717442932 Sign-in API started, Directory and File created.

---
+++ public_html/api/sign-in/sign-in.php
@@ -0,0 +1,19 @@
+<?php
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\Kindhub\Profile;
+/**
+ * api for handling sign-in
+ *
+ * @author Jermain Jennings
+ **/
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	//start session
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
+	}
