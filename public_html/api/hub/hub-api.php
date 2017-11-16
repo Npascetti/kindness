8e2898850217d5635cc0a3386b2749441fc56358
Calder Benjamin <calderbenjamin@gmail.com>
2017-11-16 13:14:14 -0700

    Created set up for the api https://app.asana.com/0/464075553520310/481713717442929

--- public_html/api/hub/hub-api.php
+++ public_html/api/hub/hub-api.php
@@ -6,7 +6,6 @@ require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\KindHub\ {
-	User,
 	Hub
 };
 
@@ -15,3 +14,36 @@ use Edu\Cnm\KindHub\ {
  *
  * @author Calder Benjamin <calderbenjamin@gmail.com>
  */
+
+//Verify the session is active, and start it if not
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// Prepares an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/kindness.ini");
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+	$hubId = filter_input(INPUT_GET, "hubId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$hubUserId = filter_input(INPUT_GET, "hubUserId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$hubLocation = filter_input(INPUT_GET, "hubLocation", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$hubName = filter_input(INPUT_GET, "hubName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	//make sure the id is valid for methods that require it
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+		throw(new InvalidArgumentException("hubId cannot be empty or negative", 405));
+	}
+
+	if($method === "GET") {
+		setXsrfCookie();
+	}
+}
