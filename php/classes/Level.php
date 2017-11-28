39eec793821409210946b86dcfa356b0448d7942
Calder Benjamin <calderbenjamin@gmail.com>
2017-11-28 16:28:25 -0700

    Fixed namespace

--- php/classes/Level.php
+++ php/classes/Level.php
@@ -1,8 +1,6 @@
 <?php
 
-//TODO add vendor autoloader
-
-namespace Edu\Cnm\kindHub;
+namespace Edu\Cnm\KindHub;
 require_once("autoload.php");
 require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 use Ramsey\Uuid\Uuid;
