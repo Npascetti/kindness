c6e95bf81aa34618218fcfe6ceeb2ff43eb441e9
Calder Benjamin <calderbenjamin@gmail.com>
2017-11-29 09:07:59 -0700

    Uncommented pieces of code commented for debugging

--- public_html/api/hub/index.php
+++ public_html/api/hub/index.php
@@ -52,39 +52,39 @@ try {
 		if(empty($hubId) === false) {
 			$hub = Hub::getHubByHubId($pdo, $hubId);
 			if($hub !== null) {
-//				$reputation = Reputation::getReputationByReputationHubId($pdo, $hubId);
-//				$data = [];
-//				$data[] = (object)[
-//					"hub" => $hub,
-//					"reputation" => $reputation
-//				];
-				$reply->data = $hub;
+				$reputation = Reputation::getReputationByReputationHubId($pdo, $hubId);
+				$data = [];
+				$data[] = (object)[
+					"hub" => $hub,
+					"reputation" => $reputation
+				];
+				$reply->data = $data;
 			}
 		}else if(empty($hubUserId) === false) {
 			$hubs = Hub::getHubsByHubUserId($pdo, $hubUserId)->toArray();
 			if($hubs !== null) {
-//				$data = [];
-//				foreach($hubs as $hub) {
-//					$reputation = Reputation::getReputationByReputationHubId($pdo, $hub->getHubId());
-//					$data[] = (object)[
-//						"hub" => $hub,
-//						"reputation" => $reputation
-//					];
-//				}
-				$reply->data = $hubs;
+				$data = [];
+				foreach($hubs as $hub) {
+					$reputation = Reputation::getReputationByReputationHubId($pdo, $hub->getHubId());
+					$data[] = (object)[
+						"hub" => $hub,
+						"reputation" => $reputation
+					];
+				}
+				$reply->data = $data;
 			}
 		} else if(empty($hubName) === false) {
 			$hubs = Hub::getHubsByHubName($pdo, $hubName)->toArray();
 			if($hubs !== null) {
-//				$data = [];
-//				foreach($hubs as $hub) {
-//					$reputation = Reputation::getReputationByReputationHubId($pdo, $hub->getHubId());
-//					$data[] = (object)[
-//						"hub" => $hub,
-//						"reputation" => $reputation
-//					];
-//				}
-				$reply->data = $hubs;
+				$data = [];
+				foreach($hubs as $hub) {
+					$reputation = Reputation::getReputationByReputationHubId($pdo, $hub->getHubId());
+					$data[] = (object)[
+						"hub" => $hub,
+						"reputation" => $reputation
+					];
+				}
+				$reply->data = $data;
 			}
 		} else {
 			$hubs = Hub::getAllHubs($pdo)->toArray();
@@ -97,7 +97,7 @@ try {
 						"reputation" => $reputation
 					];
 				}
-				$reply->data = $hubs;
+				$reply->data = $data;
 			}
 		}
 	}
