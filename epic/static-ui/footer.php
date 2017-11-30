6ebbe9dd64371221d3feee405d9458e1a06c84f4
Marcus Caldeira <mac.caldr@gmail.com>
2017-11-30 13:25:41 -0700

    Added sign up ul https://app.asana.com/0/467416618315421/481713717442936

---
+++ epic/static-ui/footer.php
@@ -0,0 +1,9 @@
+<footer class="bg-dark py-3">
+    <div class="container">
+        <div class="row">
+            <div class="col text-center text-white">
+                Couple Links to Develop
+            </div>
+        </div>
+    </div>
+</footer>
