32f5b434c298b5035f291257575f465ee0106f3d
Marcus Caldeira <mac.caldr@gmail.com>
2017-11-30 13:24:38 -0700

    Added sign up ul https://app.asana.com/0/467416618315421/481713717442936

---
+++ epic/static-ui/homeview.php
@@ -0,0 +1,53 @@
+<!-- ------------------------------------------------ -->
+<!-- This is the main template for the Home Page View -->
+<!-- ------------------------------------------------ -->
+<!DOCTYPE html>
+<html>
+
+<!-- inject head-utils -->
+<?php require_once ("head-utils.php");?>
+
+<body class="sfooter">
+<div class="sfooter-content">
+
+    <!-- inject navbar -->
+    <?php require_once("navbar.php"); ?>
+
+    <!-- begin Home Page layout -->
+    <main class="my-5">
+        <div class="container-fluid">
+
+            <!-- Page Title -->
+            <div class="row mb-3">
+                <div class="col">
+                    <h1>KindHub</h1>
+                    <hr>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6 col-lg-3">
+
+                    <!-- inject admin panel -->
+                    <?php require_once ("adminpanel.php");?>
+
+                </div>
+                <div class="col-md-6 col-lg-9">
+
+                    <!-- inject content panel -->
+                    <?php require_once ("content-panel.php");?>
+
+                </div>
+            </div>
+        </div>
+    </main>
+
+</div>
+
+<!-- inject footer -->
+<?php require_once ("footer.php");?>
+
+<!-- inject modal window -->
+<?php require_once ("signup-modal.php");?>
+</body>
+</html>
