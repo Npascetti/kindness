8751374c31aa0bb84a33bdd69171646738064a1a
Marcus Caldeira <mac.caldr@gmail.com>
2017-11-30 13:26:49 -0700

    Added sign up ul https://app.asana.com/0/467416618315421/481713717442936

---
+++ epic/static-ui/content-panel.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <div class="card-header">
+        Featured
+    </div>
+    <div class="card-body">
+        <h4 class="card-title">Hub List</h4>
+        <p class="card-text">Injectable Hub Data</p>
+        <button class="btn btn-primary" data-toggle="modal" data-target="#signUpModal">Click Me!</button>
+    </div>
+</div>
