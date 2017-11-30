ff8286f7bcf8e239253807dd085e19869ffc761f
Marcus Caldeira <mac.caldr@gmail.com>
2017-11-30 13:22:14 -0700

    added kindness
    https://app.asana.com/0/467416618315421/481713717442936

---
+++ epic/static-ui/signin-modal.php
@@ -0,0 +1,20 @@
+
+<div class="modal fade" id="signInModal" tabindex="-1" role="dialog" aria-labelledby="signInModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="signUpModalLabel">Sign In</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Sign In to KindHub
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
