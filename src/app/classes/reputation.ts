49f80249264274d0b41551d846cd26e6dd235f7c
Calder Benjamin <calderbenjamin@gmail.com>
2017-12-03 16:57:49 -0700

    Added reputation class in the src/app directory

---
+++ src/app/classes/reputation.ts
@@ -0,0 +1,9 @@
+export class Reputation {
+	constructor(
+		public reputationId: string,
+		public reputationHubId: string,
+		public reputationLevelId: string,
+		public reputationUserId: string,
+		public reputationPoint: number,
+	)
+}
