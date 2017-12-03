071d41fecf54899e7c27e762f2974eca2f1a55f6
Nicklas Pascetti <Npascetti@gmail.com>
2017-12-03 16:57:01 -0700

    created user.ts class

---
+++ src/app/classes/user.ts
@@ -0,0 +1,13 @@
+export class User {
+	constructor(
+		public userId: string,
+		public userBio: string,
+		public userEmail: string,
+		public userFirstName: string,
+		public userImage: string,
+		public userLastName: string,
+		public userPassword: string,
+		public userPasswordConfirm: string,
+		public userUserName: string,
+	) {}
+}
