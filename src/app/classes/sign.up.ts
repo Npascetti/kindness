7053c91df207c967c15852949d016e6e44ab2a2a
Marcus Caldeira <mac.caldr@gmail.com>
2017-12-03 16:59:12 -0700

    added some more stuff to sign-up

--- src/app/classes/sign.up.ts
+++ src/app/classes/sign.up.ts
@@ -1,8 +1,14 @@
 export class SignUp {
     constructor(
         public profileUserName: string,
+        public userFirstName: string,
+        public userLastName: string,
         public profileEmail: string,
         public profilePassword: string,
-        public profilePasswordConfirm: string
+        public profilePasswordConfirm: string,
+        public userImage: string,
+        public userBio: string,
+
+
     ) {}
 }
