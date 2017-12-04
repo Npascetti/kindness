99343e0c49630080a36792777c33f9baf344a560
Calder Benjamin <calderbenjamin@gmail.com>
2017-12-04 16:41:37 -0700

    created reputation service

---
+++ src/app/services/reputation.service.ts
@@ -0,0 +1,51 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Status} from "../classes/status";
+import {Reputation} from "../classes/reputation";
+import {Observable} from "rxjs/Observable";
+
+@Injectable ()
+export class ReputationService {
+
+	constructor(protected http : HttpClient ) {
+
+	}
+
+	//define the API endpoint
+	private reputationUrl = "/api/reputation/";
+
+
+	//call the reputation API and create a new reputation
+	createReputation(reputation : Reputation) : Observable<Status> {
+		return (this.http.post<Status>(this.reputationUrl, reputation));
+	}
+
+	// call to the reputation API and delete the reputation in question
+	deleteReputation(reputationId: string) : Observable<Status> {
+		return(this.http.delete<Status>(this.reputationUrl + reputationId));
+	}
+
+	// call to the hub API and edit the hub in question
+	editHub(hub : Hub) : Observable<Status> {
+		return(this.http.put<Status>(this.hubUrl + hub.hubId, hub));
+	}
+
+	//grabs a  reputation based on its id
+	getReputation(reputationId : string) : Observable <Reputation> {
+		return (this.http.get<Reputation>(this.reputationUrl+ "?reputationUserId=" + reputationUserId +"&reputationTweetId=" + reputationTweetId))
+	}
+
+	//grabs a reputation based on its hub id
+	//TODO ask george about getting this as an int
+	getReputationByReputationHubId (reputationHubId : string) : Observable<Reputation> {
+		return(this.http.get<Reputation>(this.reputationUrl + reputationHubId))
+	}
+
+	getRepuationByReputationLevelId(reputationLevelId : string) : Observable<Reputation[]> {
+		return(this.http.get<Reputation[]>(this.reputationUrl + reputationLevelId))
+	}
+
+	getReputationByReputationUserId(reputationUserId : string) : Observable<Reputation[]> {
+		return(this.http.get<Reputation[]>(this.reputationUrl + reputationUserId))
+	}
+}
