50c2330c81c96de563d288f6c9dd20184e95105a
Michael Romero <m.romero1989@gmail.com>
2017-12-05 10:43:08 -0700

    Added profile page + edit profile modal.

---
+++ epic/static-ui/profile-page.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+		<!-- inject head-utils -->
+		<?php require_once ("head-utils.php");?>
+
+	<body>
+		<div class="container"
+			  <div class="row">
+				  <div class="card mx-auto p-3 mt-3" style="width: 20rem;">
+					  <img class="card-img-top" src="../images/robert.jpg" alt="Card image cap">
+					  <div class="card-body">
+						  <h4 class="card-title">Username</h4>
+						  <p class="card-text">Bio: Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					  </div>
+					  <ul class="list-group list-group-flush">
+						  <li class="list-group-item">First Name</li>
+						  <li class="list-group-item">Last Name</li>
+						  <li class="list-group-item">Email</li>
+					  </ul>
+					  <div class="card-body">
+						  <a href="#" class="card-link" data-toggle="modal" data-target="#editProfileModal">Edit Profile</a>
+						  <a href="#" class="card-link">Create Hub</a>
+						  <a href="#" class="card-link">Delete Hub</a>
+					  </div>
+				  </div>
+			  </div>
+		<!-- inject modal window -->
+		<?php require_once ("edit-profile-modal.php");?>
+	</body>
+</html>
