4f884f8b72ae17225e2b78415f94b5d48f4064a3
Calder Benjamin <calderbenjamin@gmail.com>
2017-12-06 11:11:43 -0700

    Completed hub-panel component and html

--- src/app/components/hub.panel.component.ts
+++ src/app/components/hub.panel.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {Hub} from "../classes/hub";
 import {HubService} from "../services/hub.service";
-import {ActivatedRoute, Params} from "@angular/router";
 import 'rxjs/add/operator/switchMap';
 
 
@@ -12,17 +11,11 @@ import 'rxjs/add/operator/switchMap';
 
 export class HubPanelComponent implements OnInit {
 
-	hubs: Hub[] = [];
-	constructor(private hubService: HubService, private activatedRoute: ActivatedRoute) {}
+	hubs: Object[] = [];
+	constructor(private hubService: HubService) {}
 
 	getAllHubs(): void {
-		this.activatedRoute.params
-			.switchMap((params: Params) => this.hubService.getAllHubs())
-			.subscribe(hubs => {
-				hubs.map(hub => {
-				});
-				this.hubs = hubs;
-			});
+		this.hubService.getAllHubs().subscribe(hubs => this.hubs = hubs);
 	}
 
 	ngOnInit(): void {
