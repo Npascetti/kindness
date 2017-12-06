7bc1e1e7b58d9f5607e609e782b837b2e136777e
Nicklas Pascetti <Npascetti@gmail.com>
2017-12-06 10:39:15 -0700

    started working on edit profile component

--- src/app/components/edit.profile.component.ts
+++ src/app/components/edit.profile.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {Status} from "../classes/status";
 
 
 @Component({
@@ -6,5 +7,11 @@ import {Component} from "@angular/core";
 	selector: "edit-profile"
 })
 export class EditProfileComponent{
+	@ViewChild("editProfile") editProfile: any;
+
+	editProfile: EditProfile = new EditProfile(null, null, null, null, null, null);
+	status: Status = null;
+
+
 
 }
