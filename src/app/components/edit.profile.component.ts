d9ba9d17e9efa85c034f4315a16971724bbf6626
Nicklas Pascetti <Npascetti@gmail.com>
2017-12-06 09:40:53 -0700

    added component and template for edit profile

---
+++ src/app/components/edit.profile.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+	selector: "edit-profile",
+	templateUrl: "./templates/edit-profile-modal.html"
+})
+export class EditProfileComponent{
+
+}
