7e861800e31ff6f5c51353b20cc281af707d39f0
Calder Benjamin <calderbenjamin@gmail.com>
2017-12-06 08:29:02 -0700

    Got hub panel set up

--- src/app/components/hub.panel.component.ts
+++ src/app/components/hub.panel.component.ts
@@ -1,10 +1,24 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Hub} from "../classes/hub";
+import {HubService} from "../services/hub.service";
 
 @Component({
 	templateUrl: "./templates/hub-panel.html",
 	selector: "hub-panel"
 })
 
-export class HubPanelComponent {
+export class HubPanelComponent implements OnInit {
 
+	hubs: Hub[] = [];
+
+	constructor(private hubService: HubService) {}
+
+	getAllHubs(): void {
+		this.hubService.getAllHubs()
+			.subscribe(hubs => this.hubs = hubs);
+	}
+
+	ngOnInit(): void {
+		this.getAllHubs();
+	}
 }
