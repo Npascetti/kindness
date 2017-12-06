39c8c2f79d7901acdc05654b97d917e8d71fc711
Calder Benjamin <calderbenjamin@gmail.com>
2017-12-06 12:27:11 -0700

    Created wiring for create hub panel

--- src/app/components/create.hub.modal.component.ts
+++ src/app/components/create.hub.modal.component.ts
@@ -1,10 +1,34 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Status} from "../classes/status";
+import {HubService} from "../services/hub.service";
+import {Hub} from "../classes/hub";
 
 @Component({
 	templateUrl: "./templates/create-hub-modal.html",
 	selector: "create-hub"
 })
 
-export class CreateHubModalComponent {
+export class CreateHubModalComponent implements OnInit{
+	createHubForm: FormGroup;
+	status: Status = null;
 
+	constructor(private formBuilder: FormBuilder, private hubService: HubService) {}
+
+	ngOnInit(): void {
+		this.createHubForm = this.formBuilder.group({
+			hubLocation: ["", [Validators.maxLength(264), Validators.required]],
+			hubName: ["", [Validators.maxLength(128), Validators.required]]
+		});
+	}
+
+	createHub(): void {
+		let hub = new Hub(null, null, this.createHubForm.value.hubLocation, this.createHubForm.value.hubName);
+		this.hubService.createHub(hub).subscribe(status => {
+			this.status = status;
+			if(this.status.status === 200) {
+				this.createHubForm.reset();
+			}
+		});
+	}
 }
