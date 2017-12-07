81a91b3d84f6e5d9afd81b3356d5a1f1847a9c32
Nicklas Pascetti <Npascetti@gmail.com>
2017-12-06 20:48:09 -0700

    Working some more on image api. Added require more require once, namespace

--- public_html/api/image/index.php
+++ public_html/api/image/index.php
@@ -1,7 +1,12 @@
 <?php
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\KindHub\{
+	User
+};
 /**
  * Api for the cloudinary images that will magically work with out a class YATA
  *
@@ -16,13 +21,22 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 try {
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/kindness.ini");
+
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	$config = readConfig("/etc/apache2/capstone-mysql/kindness.ini");
+
+	$cloudinary = json_decode($config["cloudinary"]);
+
+	\Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" => $cloudinary->apiSecret]);
+
 	if($method === "POST") {
 		verifyXsrf();
-		$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/kindness.ini");
+
 		//cloudinary api stuff
-		$config = readConfig("/etc/apache2/capstone-mysql/barkparkz.ini");
-		$cloudinary = json_decode($config["cloudinary"]);
+
+
 		\Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" => $cloudinary->apiSecret]);
 		//assigning variables to the user image name, MIME type, and image extension
 		$tempUserFileName = $_FILES["dog"]["tmp_name"];
