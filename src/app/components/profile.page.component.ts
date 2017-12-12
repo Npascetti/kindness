7a7c806dfb5baa006903b95c37790896ed4660be
Calder Benjamin <calderbenjamin@gmail.com>
2017-12-12 09:47:10 -0700

    Formatted spacing

--- src/app/components/profile.page.component.ts
+++ src/app/components/profile.page.component.ts
@@ -12,35 +12,36 @@ declare const $: any;
 	templateUrl: "./templates/profile-page.html"
 })
 
-export class ProfilePageComponent implements OnInit{
+export class ProfilePageComponent implements OnInit {
 
-	user:User = new User(null, null, null, null, null, null, null, null, null) ;
+	user: User = new User(null, null, null, null, null, null, null, null, null);
 
 	userData: any;
 
 	authObj: any = {};
 
-	constructor (
-		private jwtHelperService: JwtHelperService,
-		private userService: UserService,
-		private route: ActivatedRoute
-	){}
+	constructor(private jwtHelperService: JwtHelperService,
+					private userService: UserService,
+					private route: ActivatedRoute) {
+	}
 
-	ngOnInit() : void {
-			this.getUser(this.route.snapshot.params["id"]);
+	ngOnInit(): void {
+		this.getUser(this.route.snapshot.params["id"]);
 	}
 
 	getUser(id: string): void {
-			this.userService.getUser(id)
-				.subscribe((reply:any) => {
-				this.userData = reply;
-				this.user = new User(this.userData.userId, this.userData.userBio, this.userData.userEmail, this.userData.userFirstName, this.userData.userImage, this.userData.userLastName, null, null, this.userData.userUserName);
-					}
-				);
+		this.userService.getUser(id)
+			.subscribe((reply: any) => {
+					this.userData = reply;
+					this.user = new User(this.userData.userId, this.userData.userBio, this.userData.userEmail,
+						this.userData.userFirstName, this.userData.userImage, this.userData.userLastName,
+						null, null, this.userData.userUserName);
+				}
+			);
 	}
 
-	getJwtProfileId() : any {
-			this.authObj = this.jwtHelperService.decodeToken(localStorage.getItem("jwt-token"));
+	getJwtProfileId(): any {
+		this.authObj = this.jwtHelperService.decodeToken(localStorage.getItem("jwt-token"));
 	}
 }
 
