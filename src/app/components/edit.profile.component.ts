701c71d394705d2cb7d19e4a5a6eabe567245703
Calder Benjamin <calderbenjamin@gmail.com>
2017-12-12 11:44:56 -0700

    Continued to try and make edit profile work

--- src/app/components/edit.profile.component.ts
+++ src/app/components/edit.profile.component.ts
@@ -6,49 +6,53 @@ import {UserService} from "../services/user.service";
 import {ActivatedRoute, Params} from "@angular/router";
 
 
-
 @Component({
 	templateUrl: "./templates/edit-profile-modal.html",
 	selector: "edit-profile"
 })
-export class EditProfileComponent implements OnInit{
+export class EditProfileComponent implements OnInit {
 	editProfileForm: FormGroup;
 	status: Status = null;
+	userData: any;
 	editUser: User = new User(null, null, null, null, null, null, null, null, null);
 
-	constructor(private formBuilder: FormBuilder, private userService: UserService, private route: ActivatedRoute) {}
-
-	ngOnInit(): void {
-		this.route.params.forEach((params : Params) => {
-			let userId = params["id"];
-			console.log(userId);
-			this.userService.getUser(userId)
-				.subscribe(user => {
-					this.editUser = user;
-					this.editProfileForm.patchValue(this.editUser);
-				});
-		});
-		console.log(this.editUser);
-		this.editProfileForm = this.formBuilder.group({
-			userName: ["", [Validators.maxLength(128), Validators.required]],
-			firstName: ["", [Validators.maxLength(64), Validators.required]],
-			lastName: ["", [Validators.maxLength(64), Validators.required]],
-			email: ["", [Validators.maxLength(255), Validators.required]],
-			bio: ["", [Validators.maxLength(3000)]],
-		});
-		this.applyFormChanges();
+	constructor(private formBuilder: FormBuilder, private userService: UserService, private route: ActivatedRoute) {
 	}
 
-	applyFormChanges() : void {
+	ngOnInit(): void {
+		this.getUser(this.route.snapshot.params["id"]);
+		this.editProfileForm = this.formBuilder.group({
+			userName: ["", [Validators.maxLength(128)]],
+			firstName: ["", [Validators.maxLength(64)]],
+			lastName: ["", [Validators.maxLength(64)]],
+			email: ["", [Validators.maxLength(255)]],
+			bio: ["", [Validators.maxLength(3000)]],
+		});
+	}
+
+	getUser(id: string): void {
+		this.userService.getUser(id)
+			.subscribe((reply: any) => {
+				this.userData = reply;
+				this.editUser = new User(this.userData.userId, this.userData.userBio, this.userData.userEmail,
+					this.userData.userFirstName, this.userData.userImage, this.userData.userLastName,
+					null, null, this.userData.userUserName);
+				this.editProfileForm.patchValue(this.editUser);
+			});
+	}
+
+	applyFormChanges(): void {
 		this.editProfileForm.valueChanges.subscribe(values => {
 			for(let field in values) {
-				this.editProfileForm[field] = values[field];
+				this.editUser[field] = values[field];
 			}
 		});
 	}
 
-	editProfile() : void {
+	editProfile(): void {
+		this.applyFormChanges();
 		this.userService.editUser(this.editUser)
 			.subscribe(status => this.status = status);
+		console.log(this.editUser);
 	}
 }
