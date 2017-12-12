5e793926b194e65a397b7c60696a4cab732f758c
Dylan McDonald <dmcdonald21@cnm.edu>
2017-12-11 18:39:27 -0700

    beta change to extract both state variables

--- src/app/services/deep.dive.interceptor.ts
+++ src/app/services/deep.dive.interceptor.ts
@@ -16,72 +16,80 @@ import {Observable} from "rxjs/Observable";
 @Injectable()
 export class DeepDiveInterceptor implements HttpInterceptor {
 
-    /**
-     * intercept method that extracts the data or status message based on standards outlined above
-     *
-     * @param {HttpRequest<any>} request incoming HTTP request
-     * @param {HttpHandler} next outgoing handler for next interceptor
-     * @returns {Observable<HttpEvent<any>>} Observable for next interceptor to subscribe to
-     **/
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // hand off to the next interceptor
-        return (next.handle(request).map((event: HttpEvent<any>) => {
-            // if this is an HTTP Response, from Angular...
-            if (event instanceof HttpResponse && event.body !== null) {
-                // create an event to return (by default, return the same event)
-                let dataEvent = event;
+	/**
+	 * intercept method that extracts the data or status message based on standards outlined above
+	 *
+	 * @param {HttpRequest<any>} request incoming HTTP request
+	 * @param {HttpHandler} next outgoing handler for next interceptor
+	 * @returns {Observable<HttpEvent<any>>} Observable for next interceptor to subscribe to
+	 **/
+	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		// hand off to the next interceptor
+		return (next.handle(request).map((event: HttpEvent<any>) => {
+			// if this is an HTTP Response, from Angular...
+			if(event instanceof HttpResponse && event.body !== null) {
+				// create an event to return (by default, return the same event)
+				let dataEvent = event;
 
-                // if the API is successful...
-                if (event.status === 200) {
-                    // extract the JWT Header and put it in local storage
-                    if (localStorage.getItem("jwt-token") === null) {
-                        let jwtToken = event.headers.getAll("X-JWT-TOKEN");
+				// if the API is successful...
+				if(event.status === 200) {
+					// extract the JWT Header and put it in local storage
+					if(localStorage.getItem("jwt-token") === null) {
+						let jwtToken = event.headers.getAll("X-JWT-TOKEN");
 
-                        if (jwtToken !== null) {
-                            let token: string = jwtToken[0];
-                            console.log(token);
-                            localStorage.setItem("jwt-token", token.toString());
-                        }
-                    }
+						if(jwtToken !== null) {
+							let token: string = jwtToken[0];
+							console.log(token);
+							localStorage.setItem("jwt-token", token.toString());
+						}
+					}
 
-                    // extract the data or message from the response body
-                    let body = event.body;
-                    if (body.status === 200) {
-                        if (body.data) {
-                            // extract data returned from a GET request
-                            dataEvent = event.clone({body: body.data});
-                        } else if (body.message) {
-                            // extract a successful message
-                            dataEvent = event.clone({
-                                body: {
-                                    message: body.message,
-                                    status: 200,
-                                    type: "alert-success"
-                                }
-                            });
-                        }
-                    } else {
-                        // extract a failing message when the API fails
-                        dataEvent = event.clone({
-                            body: {
-                                message: body.message,
-                                status: body.status,
-                                type: "alert-danger"
-                            }
-                        });
-                    }
-                } else {
-                    // extract a failing message when the web server fails
-                    dataEvent = event.clone({
-                        body: {
-                            message: event.statusText,
-                            status: event.status,
-                            type: "alert-danger"
-                        }
-                    });
-                }
-                return (dataEvent);
-            }
-        }));
-    }
+					// extract the data or message from the response body
+					let body = event.body;
+					if(body.data && body.message) {
+						// extract everything from a request with data AND a message
+						dataEvent = event.clone({
+							body: {
+								data: body.data,
+								message: {message: body.message, status: 200, type: "alert-success"}
+							}
+						});
+					} else if(body.status === 200) {
+						if(body.data) {
+							// extract data returned from a GET request
+							dataEvent = event.clone({body: body.data});
+						} else if(body.message) {
+							// extract a successful message
+							dataEvent = event.clone({
+								body: {
+									message: body.message,
+									status: 200,
+									type: "alert-success"
+								}
+							});
+						}
+					} else {
+						// extract a failing message when the API fails
+						dataEvent = event.clone({
+							body: {
+								message: body.message,
+								status: body.status,
+								type: "alert-danger"
+							}
+						});
+					}
+				} else {
+					// extract a failing message when the web server fails
+					dataEvent = event.clone({
+						body: {
+							message: event.statusText,
+							status: event.status,
+							type: "alert-danger"
+						}
+					});
+				}
+				return (dataEvent);
+			}
+		}));
+	}
 }
